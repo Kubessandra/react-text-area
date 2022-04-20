350100f8d4bd15c7d340cc21ac829e8a7fb6f563
Kubessandra <aymeric.beudaert@gmail.com>
2022-04-20 21:53:38 +0200

    Some extra
    RTA .

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ const InputTextArea = (props) => {
   );
 }
 
+const StatusIcon = (props) => {
+  const { success } = props;
+
+  if (success) return (
+    <div className="bg-green-500 rounded-full w-8 h-8"/>
+  )
+  return (
+    <div className="bg-red-500 rounded-full w-8 h-8"/>
+  )
+}
+
 function App() {
   const [value1, setValue1] = useState('')
   const [value2, setValue2] = useState('')
@@ -25,10 +36,21 @@ function App() {
 
   return (
     <div className="flex flex-col bg-gray-100 items-center h-screen p-8">
-      <InputTextArea onChange={(e) => onTextAreaChage(e, setValue1)} value={value1}/>
-      <InputTextArea onChange={(e) => onTextAreaChage(e, setValue2)} value={value2}/>
-      <div>
-        Diff: {value1 === value2 ? 'Same value' : 'Not the same value'}
+      <div className="bg-gray-50 shadow-md p-8 rounded-md flex flex-col items-center">
+        <h1 className="m-4 font-bold">Awesome Text Area Compare ! (Dapp, or not)</h1>
+        <div className="flex flex-row">
+          <InputTextArea
+            onChange={(e) => onTextAreaChage(e, setValue1)}
+            value={value1}
+          />
+          <InputTextArea
+            onChange={(e) => onTextAreaChage(e, setValue2)}
+            value={value2}
+          />
+        </div>
+        <div className="m-4 flex justify-center">
+          <StatusIcon success={value1 === value2} />
+        </div>
       </div>
     </div>
   );
